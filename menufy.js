7234d054464552a8e3ed5d1ae4a0bec77562e5df
Alexande Daubricourt <alexr.daubricourt@gmail.com>
2017-04-04 19:34:15 +0200

    stable release

---
+++ menufy.js
@@ -0,0 +1,84 @@
+'use strict'
+
+class ContextMenu
+{
+    constructor ( structure )
+    {
+        this.structure = structure
+        this.menu = null
+        this.x = 0
+        this.y = 0
+        
+        this.showOnRightClick = true
+        this.hideDefaultContextMenu = true
+        
+        // Middleware functions
+        this.moveTo = ( x, y ) => {
+            this.menu.style.left = x + 'px'
+            this.menu.style.top = y + 'px'
+        }
+        
+        this.init = () =>
+        {
+            this.buildDOM( this.structure )
+            
+            document.oncontextmenu = (e) =>
+            {
+                this.x = e.clientX
+                this.y = e.clientY
+            
+                if (this.showOnRightClick) this.show( this.x, this.y )
+                
+                if (this.hideDefaultContextMenu) return false
+            }
+        }
+        
+        window.addEventListener('load', this.init, false)
+    }
+    
+    show ( x, y )
+    {
+        this.moveTo(x, y)
+        this.menu.style.visibility = 'visible'
+    }
+    
+    hide ()
+    {
+        this.menu.style.visibility = 'hidden'
+    }
+    
+    
+    buildDOM ( structure )
+    {
+        this.menu = document.createElement('div')
+        this.menu.style.visibility = 'hidden'
+        this.menu.style.position = 'absolute'
+        this.menu.style.zIndex = 100
+        this.menu.classList.add('menufy-menu')
+        
+        this.outerZone = document.createElement('div')
+        this.outerZone.style.background = 'grey'
+        this.outerZone.style.width = '100%'
+        this.outerZone.style.height = '100%'
+        this.outerZone.style.position = 'fixed'
+        this.outerZone.onclick = () => this.hide()
+        
+        structure.forEach( (action, index) =>
+        {
+            let act = document.createElement('span')
+            
+            // Parsing propertie into DOM attribute
+            for ( let attr in action ) { if (attr != 'action') act.setAttribute(attr, action[attr]) }
+            
+            act.classList.add('menufy-action')
+            act.style.display = 'table'
+            act.innerText = action.label
+            act.onclick = action.action
+            
+            this.menu.appendChild(act)
+        })
+        
+        document.getElementsByTagName('body')[0].appendChild(this.menu)
+        document.getElementsByTagName('body')[0].appendChild(this.outerZone)
+    }
+}
